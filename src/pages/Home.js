aa685ad63c91e83d78221c347799ebdd498bb5e1
henriB <henri.bretseau@gmail.com>
2020-10-30 16:11:53 +0100

    Route + Pages

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            Sur la Home
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/Knowledges.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Knowledges = () => {
+    return (
+        <div>
+            Compétences
+        </div>
+    );
+};
+
+export default Knowledges;
