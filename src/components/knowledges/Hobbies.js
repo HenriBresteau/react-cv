439d5c4c590aac1d7ac07dcefb44f828bcb74f6e
henriB <henri.bretseau@gmail.com>
2020-10-31 18:44:36 +0100

    Component Hobbies

--- src/components/knowledges/Hobbies.js
+++ src/components/knowledges/Hobbies.js
@@ -2,8 +2,35 @@ import React from 'react';
 
 const Hobbies = () => {
     return (
-        <div>
-            Hobbies
+        <div className=" hobbies">
+            <h3>Intéret</h3>
+            <ul>
+                <li className="hobby">
+                    <i className="fas fa-running">
+                        <span>Course à pied</span>
+                    </i>
+                </li>
+                <li className="hobby">
+                    <i className="fas fa-hiking">
+                        <span>Randonnées</span>
+                    </i>
+                </li>
+                <li className="hobby">
+                    <i className="fas fa-seedling">
+                        <span>Permaculture</span>
+                    </i>
+                </li>
+                <li className="hobby">
+                    <i className="fab fa-bitcoin">
+                        <span>Crypto-monnaies</span>
+                    </i>
+                </li>
+                <li className="hobby">
+                    <i className="fas fa-rocket">
+                        <span>Espaces</span>
+                    </i>
+                </li>
+            </ul>
         </div>
     );
 };
