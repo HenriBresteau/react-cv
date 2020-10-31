852654b17ab1a0c128be845e36e5e424c01139d3
henriB <henri.bretseau@gmail.com>
2020-10-31 18:30:09 +0100

    Component OtherSkills

--- src/components/knowledges/OtherSkills.js
+++ src/components/knowledges/OtherSkills.js
@@ -2,8 +2,30 @@ import React from 'react';
 
 const OtherSkills = () => {
     return (
-        <div>
-            Autres
+        <div className="otherSkills">
+            <h3>Autres Compétences</h3>
+            <div className="list">
+                <ul>
+                    <li><i className="fas fa-check-square">
+                        Anglais Courant</i></li>
+                    <li><i className="fas fa-check-square">
+                        GitHub</i></li>
+                    <li><i className="fas fa-check-square">
+                        SEO</i></li>
+                    <li><i className="fas fa-check-square">
+                        Méthodes agiles</i></li>
+                </ul>
+                <ul>
+                    <li><i className="fas fa-check-square">
+                        Figma</i></li>
+                    <li><i className="fas fa-check-square">
+                        Design</i></li>
+                    <li><i className="fas fa-check-square">
+                        Photoshop</i></li>
+                    <li><i className="fas fa-check-square">
+                        Montage vidéos</i></li>
+                </ul>
+            </div>
         </div>
     );
 };
