e9ec99e7afc95ba8771f163fec7d914039487e33
henriB <henri.bretseau@gmail.com>
2020-10-31 18:27:05 +0100

    Component Exp

--- src/components/knowledges/Experiences.js
+++ src/components/knowledges/Experiences.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Experiences = () => {
     return (
-        <div>
-            Expéreicnes
+        <div className="experience">
+            <h3>Experiences</h3>
+            <div className="exp-1">
+                <h4>Directeur Technique -Apple</h4>
+                <h5>2018 - 2020</h5>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita, magnam. Voluptatem atque, dignissimos deserunt sit velit ipsum amet placeat sunt quas corporis autem quam tempore dicta distinctio officiis nesciunt quibusdam.</p>
+            </div>
+            <div className="exp-2">
+                <h4>Directeur Technique -Apple</h4>
+                <h5>2018 - 2020</h5>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita, magnam. Voluptatem atque, dignissimos deserunt sit velit ipsum amet placeat sunt quas corporis autem quam tempore dicta distinctio officiis nesciunt quibusdam.</p>
+            </div>
         </div>
     );
 };
